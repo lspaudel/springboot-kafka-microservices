18260d5aa048e98737e3afdeeecb524f7f099410
Laxman Paudel <laxmansharmapaudel@gmail.com>
2025-07-20 19:49:28 -0700

    Initial commit

---
+++ base-domains/src/test/java/net/kafkajava/base_domains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kafkajava.base_domains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
